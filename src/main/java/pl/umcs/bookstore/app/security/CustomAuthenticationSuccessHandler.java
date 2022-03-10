b9c95dfa81c128467a8d17adcb933b53adb3aae2
Robert Krzywina <robert.krzywina@gmail.com>
2022-03-10 11:47:57 +0100

    Create shoppingCart session after success login

--- src/main/java/pl/umcs/bookstore/app/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/pl/umcs/bookstore/app/security/CustomAuthenticationSuccessHandler.java
@@ -3,10 +3,15 @@ package pl.umcs.bookstore.app.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import pl.umcs.bookstore.app.book.domain.Book;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.LinkedList;
 
 @Slf4j
 class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -15,6 +20,13 @@ class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessH
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException {
         log.info("User {} has been logged in", auth.getName());
         clearAuthenticationAttributes(request);
+        createShoppingCartSession();
         response.sendRedirect("/bookstore");
+    }
+
+    private void createShoppingCartSession() {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpSession session = servletRequestAttributes.getRequest().getSession(true);
+        session.setAttribute("shoppingCart", new LinkedList<Book>());
     }
 }
